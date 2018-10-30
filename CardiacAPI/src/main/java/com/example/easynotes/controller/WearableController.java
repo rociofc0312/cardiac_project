734ca0759d66320c5bfc370fb7a24444a11e1dad
miguelb10 <miguel.blas_03@hotmail.com>
2018-10-29 23:54:47 -0500

    Commit 28
    Se agrego el envio de correo usando smtp - 29/10/2018

--- CardiacAPI/src/main/java/com/example/easynotes/controller/WearableController.java
+++ CardiacAPI/src/main/java/com/example/easynotes/controller/WearableController.java
@@ -19,63 +19,90 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.easynotes.model.Notificacion;
+import com.example.easynotes.model.Relacion;
 import com.example.easynotes.model.Usuario;
 import com.example.easynotes.model.Wearable;
 import com.example.easynotes.repository.INotificacionDao;
+import com.example.easynotes.repository.IRelacionDao;
 import com.example.easynotes.repository.IUsuarioDao;
 import com.example.easynotes.repository.IWearableDao;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @RestController
 @RequestMapping("/api")
 public class WearableController {
-		
-		@Autowired
-		IWearableDao wearableDao;
-		@Autowired
-		IUsuarioDao usuarioDao;
-		@Autowired
-		INotificacionDao notificacionDao;
-		
-		@GetMapping
-		public String sayHello() {
-		        return "Hello and Welcome to the EasyNotes application. You can create a new Note by making a POST request to /api/notes endpoint.";
-		        }
-		
-		@GetMapping("/usuarios/{dni}/{contrasenia}")
-		public Usuario obtenerUsuario(@PathVariable String dni, @PathVariable String contrasenia) {
-			Usuario usuario = usuarioDao.findByDniAndContrasenia(dni, contrasenia);
-			return usuario;
+
+	@Autowired
+	private JavaMailSender mailSender;
+	@Autowired
+	IWearableDao wearableDao;
+	@Autowired
+	IUsuarioDao usuarioDao;
+	@Autowired
+	INotificacionDao notificacionDao;
+	@Autowired
+	IRelacionDao relacionDao;
+
+	@GetMapping
+	public String sayHello() {
+		return "Hello and Welcome to the EasyNotes application. You can create a new Note by making a POST request to /api/notes endpoint.";
+	}
+
+	@GetMapping("/usuarios/{dni}/{contrasenia}")
+	public Usuario obtenerUsuario(@PathVariable String dni, @PathVariable String contrasenia) {
+		Usuario usuario = usuarioDao.findByDniAndContrasenia(dni, contrasenia);
+		return usuario;
+	}
+
+	@PostMapping("/usuarios/{id}/medidas")
+	public Wearable crearMedida(@PathVariable String id, @Valid @RequestBody Wearable wearable) throws ParseException {
+		Usuario usuario = usuarioDao.findById(Integer.parseInt(id));
+		List<Relacion> listaDeDoctores = relacionDao.findByUsuarioPacienteId(Integer.parseInt(id));
+
+		/*
+		 * SimpleDateFormat sdfDate = new
+		 * SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); Date now = new Date();
+		 * String strDate = sdfDate.format(now);
+		 * wearable.setFecha(sdfDate.parse(strDate));
+		 */
+		SimpleMailMessage message = new SimpleMailMessage();
+		wearable.setUsuario(usuario);
+
+		if (wearable.getRitmoCardiaco() > 65) {
+			for (Relacion doctor : listaDeDoctores) {
+				message.setText("El usuario" + wearable.getUsuario().getNombre() + " "
+						+ wearable.getUsuario().getApellido() + "Presenta una presion alta");
+				message.setTo(doctor.getUsuarioDoctor().getCorreo());
+				message.setFrom("Cardiac Alert");
+				message.setSubject("Alerta de presion alta");
+				mailSender.send(message);
+			}
 		}
-		
-		@PostMapping("/usuarios/{id}/medidas")
-		public Wearable crearMedida(@PathVariable String id, @Valid @RequestBody Wearable wearable) throws ParseException {
-			Usuario usuario = usuarioDao.findById(Integer.parseInt(id));
-			
-			/*SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			Date now = new Date();
-			String strDate = sdfDate.format(now);
-			wearable.setFecha(sdfDate.parse(strDate));*/
-			wearable.setUsuario(usuario);
-			//usuario = usuarioDao.findById(wearable.getUsuario().getId()).get(); 
-			//wearable.setUsuario(usuario);
-			return wearableDao.save(wearable);
-		}
-		
-		@GetMapping("/usuarios/{id}/medidas")
-		public List<Wearable> verMedidas(@PathVariable String id) {
-			Usuario usuario = usuarioDao.findById(Integer.parseInt(id));
-			return wearableDao.findByUsuario(usuario);
-		}
-		
-		@GetMapping("/medidas")
-		public  List<Wearable> getAllWearables(){
-			return (List<Wearable>) wearableDao.findAll();
-		}
-		
-		@PostMapping("/usuarios/{id}/notificaciones")
-		public Notificacion crearNotificacion(@PathVariable String id, @Valid @RequestBody Notificacion notificacion) throws ParseException {
-			Usuario usuarioPaciente = usuarioDao.findById(Integer.parseInt(id));
-			notificacion.setUsuarioPaciente(usuarioPaciente);
-			return notificacionDao.save(notificacion);
-		}
+		// usuario = usuarioDao.findById(wearable.getUsuario().getId()).get();
+		// wearable.setUsuario(usuario);
+		return wearableDao.save(wearable);
+	}
+
+	@GetMapping("/usuarios/{id}/medidas")
+	public List<Wearable> verMedidas(@PathVariable String id) {
+		Usuario usuario = usuarioDao.findById(Integer.parseInt(id));
+		return wearableDao.findByUsuario(usuario);
+	}
+
+	@GetMapping("/medidas")
+	public List<Wearable> getAllWearables() {
+		return (List<Wearable>) wearableDao.findAll();
+	}
+
+	@PostMapping("/usuarios/{id}/notificaciones")
+	public Notificacion crearNotificacion(@PathVariable String id, @Valid @RequestBody Notificacion notificacion)
+			throws ParseException {
+		Usuario usuarioPaciente = usuarioDao.findById(Integer.parseInt(id));
+		notificacion.setUsuarioPaciente(usuarioPaciente);
+		return notificacionDao.save(notificacion);
+	}
 }
--- CardiacApp/src/main/java/com/pe/cardiac/app/controller/IndexController.java
+++ CardiacApp/src/main/java/com/pe/cardiac/app/controller/IndexController.java
@@ -4,12 +4,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
+
 @Controller
 public class IndexController {
 	
+
+    @Autowired private JavaMailSender mailSender;
+    
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
 		return "redirect:/usuario/login";
 	}
+	
+	@RequestMapping(path = "/email", method = RequestMethod.GET)
+    public String sendMail() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setText("Hello from Spring Boot Application");
+        message.setTo("miguel.blas_03@hotmail.com");
+        message.setFrom("Cardiac");
+        message.setSubject("Prueba");
+        try {
+            mailSender.send(message);
+            return "Email enviado com sucesso!";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Erro ao enviar email.";
+        }
+    }
 
 }
