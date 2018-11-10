86faa35976704b9cb06f98cefe21efbfa290fa4c
miguelb10 <miguel.blas_03@hotmail.com>
2018-11-10 14:43:49 -0500

    Commit 38
    Cambio de valor

--- CardiacAPI/src/main/java/com/example/easynotes/model/Wearable.java
+++ CardiacAPI/src/main/java/com/example/easynotes/model/Wearable.java
@@ -27,76 +27,98 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-@Table(name="medida")
+@Table(name = "medida")
 @EntityListeners(AuditingEntityListener.class)
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-public class Wearable implements Serializable{
-	
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+public class Wearable implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
+
 	@NotNull
 	private float ritmo;
-	
+
 	@NotNull
 	private float oxigenacion;
-	
-	@Column(columnDefinition="char(2)")
+
+	@Column(columnDefinition = "char(2)")
 	@NotEmpty
 	@NotNull
 	private String estres;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date fecha;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name="usuario_id")
+	@JoinColumn(name = "usuario_id")
 	private Usuario usuario;
-	
+
+	private boolean valor;
+
 	@PrePersist
 	public void prePersist() {
 		fecha = new Date();
 	}
-	
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
+
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public float getRitmoCardiaco() {
 		return ritmo;
 	}
+
 	public void setRitmoCardiaco(float ritmoCardiaco) {
 		this.ritmo = ritmoCardiaco;
 	}
+
 	public float getOxigenacion() {
 		return oxigenacion;
 	}
+
 	public void setOxigenacion(float oxigenacion) {
 		this.oxigenacion = oxigenacion;
 	}
+
 	public String getEstresCardiaco() {
 		return estres;
 	}
+
 	public void setEstresCardiaco(String estresCardiaco) {
 		this.estres = estresCardiaco;
 	}
+
 	public Date getFecha() {
 		return fecha;
 	}
+
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
+
+	public boolean isValor() {
+		return valor;
+	}
+
+	public void setValor(boolean valor) {
+		this.valor = valor;
+	}
+
 	public Wearable(int id, float ritmoCardiaco, float oxigenacion, String estresCardiaco, Date fecha) {
 		super();
 		this.id = id;
@@ -105,6 +127,7 @@ public class Wearable implements Serializable{
 		this.estres = estresCardiaco;
 		this.fecha = fecha;
 	}
+
 	public Wearable() {
 		super();
 	}
